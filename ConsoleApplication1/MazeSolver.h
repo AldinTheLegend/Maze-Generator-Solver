d3a12e5f74ab8e1588f5c370a65fa0189f9206e2
Aldin Jusufagic <90244948+AldinTheLegend@users.noreply.github.com>
2024-03-25 20:16:20 +0100

    Revert "upload to github"
    This reverts commit edbc6ca629ea52c7d9607eb0e48d4f6c0fc9fc55.

--- ConsoleApplication1/MazeSolver.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "MazeGenerator.h"
-
-void bredthFirstSearch(const std::vector<std::vector<Cell>>& maze, const std::pair<int, int>& entrence, const std::pair<int, int>& exit);
--- ConsoleApplication1/Rendering.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "MazeGenerator.h"
-#include "MazeSolver.h"
-
-void draw(const std::vector<std::vector<Cell>>& maze, int currentX, int currentY, int color);
